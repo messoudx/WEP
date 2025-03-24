2f6fb5199e68e8a234030af8c7687bf150526936
messoudx <messoud0450@gmail.com>
2025-03-24 22:32:40 +0300

    add

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
-const A = "Messoud"
-const B = "Benabed"
-console.log(`my name is ${A} and my last name is ${B}`);
+// const element = document.createElement("h3");
+// element.innerText = "2025";
+// document.body.appendChild(element);
+// console.log(element);
+// const myname = document.querySelector("#name");
+// myname.style.color = "blue";
+// const kil = document.querySelectorAll(".ana");
+// Array.from(kil).forEach((kiler) => {
+//   kiler.style.color = "green";
+// });
+// const btn = document.querySelector("#click");
+// btn.style.color = "red";
+// console.log(btn.dataset);
+// btn.addEventListener("click", () => {
+//   const click = btn.dataset.clicks;
+//   btn.dataset.clicks = Number(btn.dataset.clicks) + 1;
+//   console.log(`clicked here ${click}`);
+// });
